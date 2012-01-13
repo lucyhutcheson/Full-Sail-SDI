c89b9276f79763ed0c2b8e5a0de5a81d762f82b8
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2012-01-12 21:31:22 -0600

    Cleaned up unnecessary and test code.

--- Hutcheson_Lucy_assignment2.js
+++ Hutcheson_Lucy_assignment2.js
@@ -7,53 +7,6 @@
  */
 
 // Initial variables
-
-/*var pi = 3.14159;
-var radius = 5;
-
-var getArea = function (r) {
-	var area = r * r * pi;
-	return ;
-};
-
-var reportArea = function(name,area,message){
-	console.log(name + "'s circle has " + area + " area, and " + message);
-}
-
-var area = getArea(60);
-reportArea('Ceiling cat', area, 'is all seeing');
-
-*/
-/*
-var buyMilk = function(givenCash) {
-	var pricePerGallon = 3,
-		wholeGallons;
-	if (givenCash < pricePerGallon) {
-		console.log("That's not enough to buy anything!");
-		wholeGallons = 0;
-	} else { 
-		wholeGallons = Math.floor(givenCash / pricePerGallon);
-	}
-	return wholeGallons;
-};
-var gotGallons = buyMilk(1);
-console.log("Thanks for buying " + gotGallons + " gallons of milk!");
-
-
-var getGreetingByName = function(name) {
-	var currentHour = (new Date()).getHours(),
-		timeOfDay, greeting;
-	if (currentHour < 12) { timeOfDay = "morning"; }
-	else if (currentHour < 17) { timeOfDay = "afternoon"; }
-	else { timeOfDay = "evening"; }
-	greeting = "Good " + timeOfDay + ", " + name + "!";
-	return(greeting);
-};
-var fullGreeting = getGreetingByName("Lucy");
-console.log(fullGreeting);
-*/
-
-
 var dogStylist = "Emily",
 	daysWorking = 8,
 	weHaveDogs = true,
@@ -112,13 +65,4 @@ var sayMessage = function(dogStylist, dogName) {
 washAllDogs(dogNames, wholeMinutesPerDog);
 
 
-/*
-var washAllDogs = function() {
-	var dogsRemaining = dogNames.length;
-	while (dogsRemaining > 0) {
-		console.log("I have " + dogsRemaining + " number of dogs left to wash.");
-		dogsRemaining--;
-	} // for dogNumber
-}; // washAllDogs
-*/
 
