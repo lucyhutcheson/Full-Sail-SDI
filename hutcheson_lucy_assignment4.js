9988b88071ef9bbf9ee9042866924540bf6670d3
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2012-01-25 13:02:38 -0600

    Created stringLibrary with validatePhone function.

--- hutcheson_lucy_assignment4.js
+++ hutcheson_lucy_assignment4.js
@@ -15,6 +15,24 @@ var pbj = makeSandwich("white", "peanut butter", ["grape jelly"]);
 // pbj is an object that will eventually do something useful
 
 
+// STRING
+// each character in the string are represented by numerical indexes
+// index 1 = "o"
+var myString = "Some Text";
+var length = myString.length
+console.log(length);
+
+//myString.substring(from, to) in index values
+var substring = myString.substring(0,4);
+console.log(substring); // returns "Some"
+
+//myString.charAt(n) n=index will return the character
+var character = myString.charAt(3);
+console.log(character); // returns "e"
+
+// indexOf will return the index.  if no index exists, it will return -1
+var index = myString.indexOf("e");
+console.log(index); // returns 3
 
 // MATH
 // round 
@@ -41,3 +59,43 @@ console.log(Math.floor(answer));
 var answer = (Math.random() * 10) + 1;
 console.log(answer);
 console.log(Math.floor(answer));
+
+
+
+// LIBRARY - Revealing Module Pattern
+var ninjaLibrary = function () {
+    // TODO: add some private variables here
+    var throwingStars, toeShoes;
+    // TODO: add some private methods here
+    var signal = function (message) {};
+    // TODO: reveal the public methods here
+    return {
+        "signal" : signal
+    };
+};
+
+// this is how you use it
+var ninjaLib = ninjaLibrary();
+ninjaLib.signal("This is a message");
+
+var phoneNumber = "1234567890";
+
+var stringLibrary = function(phoneNumber) {
+	var validatePhone = function(phoneNumber) {
+		 if (phoneNumber.indexOf("-") === -1) {
+		 	var areaCode = phoneNumber.substring(0,3);
+			var phoneFirst = phoneNumber.substring(3,6);
+			var phoneLast = phoneNumber.substring(6,10);
+			var validNumber = (areaCode + "-" + phoneFirst + "-" + phoneLast);
+			return validNumber;
+		 } else {
+		 	return validNumber;
+		 };
+	};
+	return {
+		"validatePhone" : validatePhone
+	};
+};
+var stringLib = stringLibrary();
+stringLib.validatePhone(phoneNumber); // return validated number
+console.log(validNumber);
